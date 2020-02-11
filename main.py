66d5e96e68eade3a6eb47c906ebf5723d6801c0c
Ryan Woodworth <rswoodworth29@gmail.com>
2020-02-10 19:13:12 -0500

    Set interpreter and initial username prompt

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3  #Setting interpreter
+
+
+#Initial start welcome & prompt for username
+print('Hello, Welcome to Tix ticketing system.')  
+
+
+currentUser = input('Please enter your username to login \n'); #Prompt for user to input username *need to add error handling*
