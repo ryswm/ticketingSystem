9b4d2b74ec022bb8e19126ccb27dfd184ab78263
Ryan Woodworth <rswoodworth29@gmail.com>
2020-02-12 09:41:02 -0500

    Adding some todos

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3  #Setting interpreter
 
+#TODO: READ ACCOUNT FILE & EVENT FILE
 
 #Initial start welcome & prompt for username
 print('Hello, Welcome to Tix ticketing system.')  
 
 
 currentUser = input('Please enter your username to login \n'); #Prompt for user to input username *need to add error handling*
+#TODO: USER INPUT ERROR CHECKING
+
+#TODO: LOGIN AS currentUser
+
+#TODO: PROVIDE UI MENU FOR AVAILABLE USER FUNCTIONS
