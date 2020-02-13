13aff50e7a04a359fa74224e74231514c43d01a3
manueloshana <manuel.oshana@ryerson.ca>
2020-02-13 15:22:20 -0500

    Created mainMenu and createUser functions.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3  #Setting interpreter
 
 #TODO: READ ACCOUNT FILE & EVENT FILE
-#using content manager
-with open('AccountFile', 'r') as f:
-    f_contents = f.readline()
-    print(f_contents, end='')
-    f_contents = f.readline()
-    print(f_contents,)
 
-#TODO: LOGIN AS currentUser
+def createUser():
+    f = open("AccountFile.txt","a+")
+    users = f.read()
+    newUser = input("Please enter your desired username: \n")
+
+    if len(newUser) > 15:
+        print("Username cannot exceed 15 characters.")
+    elif newUser in users:
+        print("Sorry, this username is already taken.")
+    elif newUser not in users:
+        print("Successfully created the user, " + newUser)
+        f.write(newUser)
+
 def login():
     f = open("AccountFile.txt", "r")
     users = f.read()
@@ -20,11 +26,19 @@ def login():
         print("Username cannot exceed 15 characters.")
     elif currentUser in users:
         print("Successfully logged in as: " + currentUser)
+        mainMenu()
     elif currentUser not in users:
         print("User does not exist in the system.")
+def mainMenu():
+    print("Welcome to the Tix ticketing system, please select from the following options.")
+    selection = input("1. create \n 2. login \n 3. logout \n 4. delete \n 5. sell \n 6. buy \n 7. refund \n 8. addcredit \n")
+    if selection == "create":
+        createUser()
+    elif selection == "login":
+        login()
+    #TODO: Add the rest of the options once the functions are made
 #Initial start welcome & prompt for username
-print('Hello, Welcome to Tix ticketing system.')
-login()
+mainMenu()
 
 
 
