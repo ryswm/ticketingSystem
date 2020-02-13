5e92bd04dbbe028c568526fb1ba69f33909d303b
Jonatahn Bajada <jbajada@ryerson.ca>
2020-02-13 13:52:32 -0500

    02 - Added todo

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3  #Setting interpreter
 
 #TODO: READ ACCOUNT FILE & EVENT FILE
+f = open('AccountFile', 'r')
+f.close()
 
 #Initial start welcome & prompt for username
-print('Hello, Welcome to Tix ticketing system.')  
-
+print('Hello, Welcome to Tix ticketing system.')
 
 currentUser = input('Please enter your username to login \n'); #Prompt for user to input username *need to add error handling*
 #TODO: USER INPUT ERROR CHECKING
+#TODO: Read username againt accountfile
 #Username cannot exceed 15 characters
 if len(currentUser) > 15:
     print("too big")
