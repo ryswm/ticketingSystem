f5f08c2659832dc2d034e2a678bd010231bc2c82
manueloshana <manuel.oshana@ryerson.ca>
2020-02-13 14:53:42 -0500

    Added basic login system.

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ with open('AccountFile', 'r') as f:
     f_contents = f.readline()
     print(f_contents,)
 
+#TODO: LOGIN AS currentUser
+def login():
+    f = open("users.txt", "r")
+    users = f.read()
+    currentUser = input('Please enter your username to login \n') #Prompt for user to input username *need to add error handling*
+    #TODO: USER INPUT ERROR CHECKING
+    #TODO: Read username against accountfile
+    #Username cannot exceed 15 characters
+    if len(currentUser) > 15:
+        print("Username cannot exceed 15 characters.")
+    elif currentUser in users:
+        print("Successfully logged in as: " + currentUser)
+    elif currentUser not in users:
+        print("User does not exist in the system.")
 #Initial start welcome & prompt for username
 print('Hello, Welcome to Tix ticketing system.')
+login()
 
-currentUser = input('Please enter your username to login \n'); #Prompt for user to input username *need to add error handling*
-#TODO: USER INPUT ERROR CHECKING
-#TODO: Read username against accountfile
-#Username cannot exceed 15 characters
-if len(currentUser) > 15:
-    print("too big")
 
-#TODO: LOGIN AS currentUser
 
 #TODO: PROVIDE UI MENU FOR AVAILABLE USER FUNCTIONS
