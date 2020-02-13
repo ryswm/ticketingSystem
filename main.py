a0877bfedddcae62ba5cb66c8f208ac49d12581d
Jonatahn Bajada <jbajada@ryerson.ca>
2020-02-13 14:01:27 -0500

    3 - jbajada - Added Read function
    Added ability to read content in file and print it off to screen. So it works. Thinking we should add if statement checks on username and permissions against delete and login etc..

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3  #Setting interpreter
 
 #TODO: READ ACCOUNT FILE & EVENT FILE
-f = open('AccountFile', 'r')
-f.close()
+#using content manager
+with open('AccountFile', 'r') as f:
+    f_contents = f.readline()
+    print(f_contents, end='')
+    f_contents = f.readline()
+    print(f_contents,)
 
 #Initial start welcome & prompt for username
 print('Hello, Welcome to Tix ticketing system.')
 
 currentUser = input('Please enter your username to login \n'); #Prompt for user to input username *need to add error handling*
 #TODO: USER INPUT ERROR CHECKING
-#TODO: Read username againt accountfile
+#TODO: Read username against accountfile
 #Username cannot exceed 15 characters
 if len(currentUser) > 15:
     print("too big")
