4de04842d08d00b6e95b79ff8266eccaa46aace1
Jonatahn Bajada <jbajada@ryerson.ca>
2020-02-17 14:28:33 -0500

    02/17 - jbajada - refund
    able to update the credit for the buyer but it appears below the decimal.
    
    bounds error when subtracting a value from seller

--- main.py
+++ main.py
@@ -175,17 +175,22 @@ def refund():
     code = "05"
     buyer = input("Please enter buyer's account name. \n")
     if buyer in users:
-        seller = input("Please enter seller's account name. \n")
-        if seller in users:
-            credit = input("Please enter credit amount. \n")
-        else:
-            print("There is no seller by that name! \n")
-    else:
-        print("There is no buyer by that name! \n")
+        for i in range(len(users) - 1):
+            if users[i,0] == buyer:
+                refund = input("Please enter refund amount. \n")
+                buyercredit = users[i,2]
+                users[i, 2] = buyercredit + refund
+                print(users[i, 2])
+            seller = input("Please enter seller's account name. \n")
+            #TODO: fix subtraction bounds error
+            if seller in users:
+                for i in range(len(users) - 1):
+                    if users[i,0] == seller:
+                        refund = input("Please enter refund amount. \n")
+                        sellerCredit = users[i,2]
+                        users[i,2] = sellerCredit - refund
+                        print(users[i, 2])
 
-            #TODO: subtract credit from sellers account
-            #TODO: add credit to the buyers account
-            #TODO: update daily transactions file
 
     
 
