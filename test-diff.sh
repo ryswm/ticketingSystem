ed92d1aef2a2ba92364be4b67bab126084df9c8d
Ryan Woodworth <rswoodworth29@gmail.com>
2020-03-03 11:25:38 -0500

    testing script and difference checking script now executable from single script

--- test-diff.sh
+++ test-diff.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-testout=$(find ./Tests/expected -type f -name "*.eto")
+#Checking difference between expected and actual terminal output
+testout=$(find ./Tests/expected -type f -name "*.eto")  #Find all expected terminal output files
 for i in $testout
 do
-    name="${i##*/}"
-    base="${name%.*}"
-    actual=$(find ./Tests/outputs -type f -name "$base.ato")
-    expected=$(find ./Tests/expected -type f -name "$base.eto")
-    difference=$(diff $actual $expected)
+    name="${i##*/}"     #Get filename from path
+    base="${name%.*}"   #Get test name from filename
+    actual=$(find ./Tests/outputs -type f -name "$base.ato")    #Find corresponding actual terminal output file
+    expected=$(find ./Tests/expected -type f -name "$base.eto") #Find corresponding expected terminal output file
+    difference=$(diff $actual $expected)    #Check difference
 
-    if [ "$difference" != "" ]
+    if [ "$difference" != "" ]  #If there is a difference
     then
-        echo $difference > ./Tests/results/$base.resterm
+        echo $difference > ./Tests/results/$base.resterm    #Write difference to result file
     else
-        echo "Test $base terminal output passed"
+        echo "Test $base terminal output passed"    #Else, print test passed
     fi
 done
 
-testtran=$(find ./Tests/expected -type f -name "*.edt")
+#Checking difference between expected and actual transaction file output
+testtran=$(find ./Tests/expected -type f -name "*.edt") #Find all expected transaction output files
 for i in $testtran
 do
     name="${i##*/}"
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./tix.sh
+./test-diff.sh
