501fa18ce47dec7590659d7c6ac5906685fb5289
Jonatahn Bajada <jbajada@ryerson.ca>
2020-03-29 12:47:59 -0400

    Update eventTests.java

--- ticketingSystem/src/eventTests.java
+++ ticketingSystem/src/eventTests.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
+
 
 public class eventTests extends Backend{
 
@@ -6,9 +8,10 @@ public class eventTests extends Backend{
     @Test
     public void shouldGetEventName(){
         String name = "billy idol         admin           100 001.00";
+        String nameTrue = "billy idol";
         tickets.add(name);
         Backend.getEventNames();
-        System.out.println(Backend.eventNames);
+        assertTrue(Backend.eventNames.contains(nameTrue));
     }
 
 
